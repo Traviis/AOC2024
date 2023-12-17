3cff3c9faa55dee75039b8620db7c0fe6f0932ce
Travis Taylor <travis@trickfire.com>
2023-12-17 15:46:24 -0700

    wip: Add day17 input

--- src/day17.rs
+++ src/day17.rs
@@ -1,10 +1,16 @@
+use std::collections::HashMap;
 
-type InputType = Vec<&str>;
+type Coordinate = (i64,i64);
+type InputType = HashMap<Coordinate,i64>;
 type OutputType = u64;
 
 #[aoc_generator(day17)]
 fn day17_parse(input: &str) -> InputType {
-    todo!();
+    input.lines().enumerate().flat_map(|(y, line)| {
+        line.chars().enumerate().map(move |(x, c)| {
+            ((x as i64,y as i64),c.to_digit(10).unwrap() as i64)
+        })
+    }).collect()
 }
 
 #[aoc(day17, part1)]
@@ -23,7 +29,19 @@ mod tests {
     use super::*;
 
     fn get_test_input() -> &'static str {
-        todo!();
+        "2413432311323
+3215453535623
+3255245654254
+3446585845452
+4546657867536
+1438598798454
+4457876987766
+3637877979653
+4654967986887
+4564679986453
+1224686865563
+2546548887735
+4322674655533"
     }
 
     #[test]
