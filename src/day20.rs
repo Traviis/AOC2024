b49457b4f7c164507c050468cd2d924d68f14ed2
Travis Taylor <travis@trickfire.com>
2023-12-28 20:25:07 -0700

    feat: day 20 part2 finish

--- src/day20.rs
+++ src/day20.rs
@@ -10,9 +10,19 @@ pub enum Module {
     FlipFlop(Pulse, Vec<String>),
     Conjunction(Vec<(String, Pulse)>, Vec<String>), //default low, remember last input FROM EACH INPUT
 }
+
+impl Module {
+    fn dests(self: &Self) -> &Vec<String> {
+        match self {
+            Module::Broadcaster(dests) => dests,
+            Module::FlipFlop(_, dests) => dests,
+            Module::Conjunction(_, dests) => dests,
+        }
+    }
+}
 //process in the order they are sent
 
-#[derive(Debug, Clone, Copy,PartialEq,Eq)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum Pulse {
     Low,
     High,
@@ -74,7 +84,6 @@ fn day20_parse(input: &str) -> InputType {
                         inputs.push((name2.clone(), Pulse::Low));
                     }
                 }
-                _ => (),
             });
         }
     });
@@ -82,158 +91,202 @@ fn day20_parse(input: &str) -> InputType {
     modules
 }
 
-fn count_pulses(input: &InputType, presses: i64) -> OutputType {
+#[aoc(day20, part1)]
+pub fn part1(input: &InputType) -> OutputType {
     let mut modules = input.clone(); //State is stored in the map
 
     let mut queue = VecDeque::new();
     //Queue is: destination_name, pulse_type, and source_name
+    let presses = 1000;
 
     let mut low_pulses = presses; //From the button press
     let mut high_pulses = 0;
 
     let broadcast_node = modules.get("broadcaster").unwrap().clone();
 
-    for pressed in 0..presses {
-        let dests = match broadcast_node {
-            Module::Broadcaster(ref dests) => dests,
-            _ => unreachable!(),
-        };
-        for dest in dests.iter() {
-            low_pulses += 1;
-            queue.push_back((dest.clone(), Pulse::Low, "broadcaster".to_string())); //Send low pulse to all
-        }
+    let mut seen = HashMap::new();
+    let mut cycle_length = HashMap::new();
 
-        while let Some((name, pulse, source)) = queue.pop_front() {
-            let modu = modules.get_mut(&name);
-            if modu.is_none() {
-                continue;
-            }
-
-            match modu.unwrap() {
-                Module::Broadcaster(_) => unreachable!(),
-                Module::FlipFlop(state, dests) => {
-                    if pulse == Pulse::Low {
-                        //Low pulse
-                        *state = match *state {
-                            Pulse::Low => Pulse::High,
-                            Pulse::High => Pulse::Low,
-                        };
-                        for dest in dests.iter() {
-                            match *state {
-                                Pulse::High => high_pulses += 1,
-                                Pulse::Low => low_pulses += 1,
-                            };
-                            queue.push_back((dest.clone(), *state, name.clone()));
-                        }
-                    } else {
-                        //High pulse are ignored
-                    }
-                }
-                Module::Conjunction(sources, dests) => {
-                    //Update the remembered values
-                    sources.iter_mut().for_each(|(name, state)| {
-                        if *name == source {
-                            *state = pulse;
-                        }
-                    });
-                    let all_high = sources.iter().all(|(_, state)| *state == Pulse::High);
-                    
-                    let pulse_to_send = if all_high { Pulse::Low } else { Pulse::High };
-
-                    for dest in dests.iter() {
-                        match pulse_to_send {
-                            Pulse::High => high_pulses += 1,
-                            Pulse::Low => low_pulses += 1,
-                        };
-                        queue.push_back((dest.clone(), pulse_to_send, name.clone()));
-                    }
-                }
-            }
-        }
+    for press in 0..presses {
+        propogate(
+            press as u64,
+            &broadcast_node,
+            &mut modules,
+            &mut queue,
+            &mut high_pulses,
+            &mut low_pulses,
+            &mut cycle_length,
+            &mut seen,
+            "",
+        );
     }
 
     (low_pulses * high_pulses) as u64
 }
 
-#[aoc(day20, part1)]
-pub fn part1(input: &InputType) -> OutputType {
-    count_pulses(input, 1000)
+fn propogate(
+    cycles: u64,
+    broadcast_node: &Module,
+    modules: &mut InputType,
+    queue: &mut VecDeque<(String, Pulse, String)>,
+    high_pulses: &mut i64,
+    low_pulses: &mut i64,
+    cycle_length: &mut HashMap<String, u64>,
+    seen: &mut HashMap<String, u64>,
+    conjunction_name: &str,
+) {
+    let dests = match broadcast_node {
+        Module::Broadcaster(ref dests) => dests,
+        _ => unreachable!(),
+    };
+    for dest in dests.iter() {
+        *low_pulses += 1;
+        queue.push_back((dest.clone(), Pulse::Low, "broadcaster".to_string())); //Send low pulse to all
+    }
+
+    while let Some((name, pulse, source)) = queue.pop_front() {
+        let modu = modules.get_mut(&name);
+        if modu.is_none() {
+            continue;
+        }
+
+        //Are we a conjunction_feeder?
+        if pulse == Pulse::High && conjunction_name == name {
+            //we only care about high pulses
+            if let Some(existing_cycle) = cycle_length.get(&source) {
+                //We are a conjunction feeder, so we need to check if we've seen this pulse before
+                if let Some(last_seen_cycle) = seen.get(&source) {
+                    let cycle_diff = cycles - last_seen_cycle;
+                    if cycle_diff != *existing_cycle {
+                        cycle_length.insert(source.clone(), cycle_diff);
+                    }
+                    //Should we check how many times we have cycled? Or just that we have? Could there be an unstable cycle?
+                }
+                seen.insert(source.clone(), cycles);
+            }
+        }
+
+        match modu.unwrap() {
+            Module::Broadcaster(_) => unreachable!(),
+            Module::FlipFlop(state, dests) => {
+                if pulse == Pulse::Low {
+                    //Low pulse
+                    *state = match *state {
+                        Pulse::Low => Pulse::High,
+                        Pulse::High => Pulse::Low,
+                    };
+                    for dest in dests.iter() {
+                        match *state {
+                            Pulse::High => *high_pulses += 1,
+                            Pulse::Low => *low_pulses += 1,
+                        };
+                        queue.push_back((dest.clone(), *state, name.clone()));
+                    }
+                } else {
+                    //High pulse are ignored
+                }
+            }
+            Module::Conjunction(sources, dests) => {
+                //Update the remembered values
+                sources.iter_mut().for_each(|(name, state)| {
+                    if *name == source {
+                        *state = pulse;
+                    }
+                });
+                let all_high = sources.iter().all(|(_, state)| *state == Pulse::High);
+
+                let pulse_to_send = if all_high { Pulse::Low } else { Pulse::High };
+
+                for dest in dests.iter() {
+                    match pulse_to_send {
+                        Pulse::High => *high_pulses += 1,
+                        Pulse::Low => *low_pulses += 1,
+                    };
+                    queue.push_back((dest.clone(), pulse_to_send, name.clone()));
+                }
+            }
+        }
+    }
 }
 
 #[aoc(day20, part2)]
 pub fn part2(input: &InputType) -> OutputType {
     //We have to assume that these cycle, find the conjunction that attachs to the rx node.
     // It will be low when all of it's inputs are high, thus, we can find the cycle length for each of the second to last nodes inputs and then find the least common multiple of them.
-    let mut modules = input.clone(); //State is stored in the map
+    //Queue is: destination_name, pulse_type, and source_name
+
+    let mut modules = input.clone();
+
+    //Find the destination that has rx
+    let feed_rx = modules
+        .iter()
+        .find(|(_, modu)| {
+            //this should be a single node, and in this case, it's one that is also a Conjunction
+            modu.dests().contains(&"rx".to_string())
+        })
+        .unwrap();
+
+    let feed_rx_name = feed_rx.0.clone();
+
+    let mut cycle_length = input
+        .iter()
+        .filter(|(_, modu)| modu.dests().contains(&feed_rx.0))
+        .map(|(name, _)| (name.clone(), 0))
+        .collect::<HashMap<String, u64>>();
+
+    //Now that we have all the conjunction feeders, let's detect cycles.
+    // Go through, and whenever we see one of the conjunction feeders get a high signal, mark that as the length
+    // If you see it again n times (via the seen queue) you know the cycle length is the one we saw
+
+    //I had to make a LOT of assumptions here, namely that there would be a cycle, and it would be
+    //a stable cycle. I only checked if it cycled a single time, which is pretty ballsy, if that
+    //didn't work, I would have had to have tried a few times to make sure that the cycle was
+    //stable, but the first cycle that appeared just happened to be correct, so I went with it.
+
+    let mut seen: HashMap<String, u64> = HashMap::new();
 
     let mut queue = VecDeque::new();
     //Queue is: destination_name, pulse_type, and source_name
 
+    let mut low_pulses = 0; //From the button press
+    let mut high_pulses = 0;
+
     let broadcast_node = modules.get("broadcaster").unwrap().clone();
 
-    let mut seen : HashMap<String, i64> = HashMap::new();
-    let mut cycle_length : HashMap<String, i64>::new() = HashMap::new();
+    let mut cycles: u64 = 0;
 
     loop {
-        let dests = match broadcast_node {
-            Module::Broadcaster(ref dests) => dests,
-            _ => unreachable!(),
-        };
-        for dest in dests.iter() {
-            queue.push_back((dest.clone(), Pulse::Low, "broadcaster".to_string())); //Send low pulse to all
-        }
-
-        while let Some((name, pulse, source)) = queue.pop_front() {
-            let modu = modules.get_mut(&name);
-            if modu.is_none() {
-                continue;
-            }
-
-            match modu.unwrap() {
-                Module::Broadcaster(_) => unreachable!(),
-                Module::FlipFlop(state, dests) => {
-                    if pulse == Pulse::Low {
-                        //Low pulse
-                        *state = match *state {
-                            Pulse::Low => Pulse::High,
-                            Pulse::High => Pulse::Low,
-                        };
-                        for dest in dests.iter() {
-                            match *state {
-                                Pulse::High => high_pulses += 1,
-                                Pulse::Low => low_pulses += 1,
-                            };
-                            queue.push_back((dest.clone(), *state, name.clone()));
-                        }
-                    } else {
-                        //High pulse are ignored
-                    }
-                }
-                Module::Conjunction(sources, dests) => {
-                    //Update the remembered values
-                    sources.iter_mut().for_each(|(name, state)| {
-                        if *name == source {
-                            *state = pulse;
-                        }
-                    });
-                    let all_high = sources.iter().all(|(_, state)| *state == Pulse::High);
-                    
-                    let pulse_to_send = if all_high { Pulse::Low } else { Pulse::High };
-
-                    for dest in dests.iter() {
-                        match pulse_to_send {
-                            Pulse::High => high_pulses += 1,
-                            Pulse::Low => low_pulses += 1,
-                        };
-                        queue.push_back((dest.clone(), pulse_to_send, name.clone()));
-                    }
-                }
-            }
+        propogate(
+            cycles,
+            &broadcast_node,
+            &mut modules,
+            &mut queue,
+            &mut high_pulses,
+            &mut low_pulses,
+            &mut cycle_length,
+            &mut seen,
+            &feed_rx_name,
+        );
+        cycles += 1;
+        //Check if all the feeders have a non zero cycle length
+        if cycle_length.values().all(|v| *v != 0) {
+            break;
         }
     }
 
+    let lcm = cycle_length.iter().map(|(_, v)| *v).fold(1, |acc, x| {
+        let gcd = gcd(acc, x);
+        acc * x / gcd
+    });
 
+    lcm
+}
 
+fn gcd(a: u64, b: u64) -> u64 {
+    if b == 0 {
+        return a;
+    }
+    gcd(b, a % b)
 }
 
 #[cfg(test)]
