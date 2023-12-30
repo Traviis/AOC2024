b3e20700112cf8667af70c9c4bdbd6073c09911a
Travis Taylor <travis@trickfire.com>
2023-12-30 16:10:08 -0700

    wip: Day23

--- src/day23.rs
+++ src/day23.rs
@@ -1,14 +1,45 @@
+use std::collections::HashMap;
 
-type InputType = Vec<&str>;
+
+type InputType = (HashMap<(i32, i32), Tile>, (usize, usize)>); //max_x, max_y
 type OutputType = u64;
+
+pub enum Tile {
+    Forest,
+    Path,
+    SlopeEast, // >
+    SlopeWest, // <
+    SlopeNorth, // ^
+    SlopeSouth, // v
+}
 
 #[aoc_generator(day23)]
 fn day23_parse(input: &str) -> InputType {
-    todo!();
+    let map = input.lines().enumerate().flat_map(|(y, line)| {
+        line.chars().enumerate().map(move |(x, c)| {
+            let tile = match c {
+                '#' => Tile::Forest,
+                '.' => Tile::Path,
+                '>' => Tile::SlopeEast,
+                '<' => Tile::SlopeWest,
+                '^' => Tile::SlopeNorth,
+                'v' => Tile::SlopeSouth,
+                _ => panic!("Invalid tile"),
+            };
+            ((x as i32, y as i32), tile)
+        })
+    }).collect();
+
+    let max_x = input.lines().next().unwrap().len();
+    let max_y = input.lines().count();
+
+    (map, max_x, max_y)
 }
 
 #[aoc(day23, part1)]
 pub fn part1(input: &InputType) -> OutputType {
+    //In the examle and in the input, you generally only have one direction you can go, there is no
+    //double wide paths, so we can probably constract the nodes and keep track of their cost
     todo!();
 }
 
@@ -23,7 +54,29 @@ mod tests {
     use super::*;
 
     fn get_test_input() -> &'static str {
-        todo!();
+        "#.#####################
+#.......#########...###
+#######.#########.#.###
+###.....#.>.>.###.#.###
+###v#####.#v#.###.#.###
+###.>...#.#.#.....#...#
+###v###.#.#.#########.#
+###...#.#.#.......#...#
+#####.#.#.#######.#.###
+#.....#.#.#.......#...#
+#.#####.#.#.#########v#
+#.#...#...#...###...>.#
+#.#.#v#######v###.###v#
+#...#.>.#...>.>.#.###.#
+#####v#.#.###v#.#.###.#
+#.....#...#...#.#.#...#
+#.#########.###.#.#.###
+#...###...#...#...#.###
+###.###.#.###v#####v###
+#...#...#.#.>.>.#.>.###
+#.###.###.#.###.#.#v###
+#.....###...###...#...#
+#####################.#"
     }
 
     #[test]
