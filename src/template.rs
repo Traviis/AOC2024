1bd42814bb2489649ae369d505a5d4882698b339
Travis Taylor <travis@trickfire.com>
2024-12-04 23:20:02 -0700

    fix: Update template to use day X instead of day 1.

--- src/template.rs
+++ src/template.rs
@@ -1,17 +1,17 @@
 type InputType = Vec<&str>;
 type OutputType = u64;
 
-#[aoc_generator(day1)]
-fn day1_parse(input: &str) -> InputType {
+#[aoc_generator(dayX)]
+fn dayX_parse(input: &str) -> InputType {
     todo!();
 }
 
-#[aoc(day1, part1)]
+#[aoc(dayX, part1)]
 pub fn part1(input: &InputType) -> OutputType {
     todo!();
 }
 
-#[aoc(day1, part2)]
+#[aoc(dayX, part2)]
 pub fn part2(input: &InputType) -> OutputType {
     todo!();
 }
@@ -26,12 +26,12 @@ mod tests {
     }
 
     #[test]
-    fn day1_part1() {
-        assert_eq!(part1(&day1_parse(get_test_input())), 0);
+    fn dayX_part1() {
+        assert_eq!(part1(&dayX_parse(get_test_input())), 0);
     }
 
     #[test]
-    fn day1_part2() {
-        assert_eq!(part2(&day1_parse(get_test_input())), 0);
+    fn dayX_part2() {
+        assert_eq!(part2(&dayX_parse(get_test_input())), 0);
     }
 }
